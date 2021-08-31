c6971bdc63ef1a9302a1cdefd4dc8b370ab802ed
carlos_jara <carlos.jara3021@gmail.com>
2021-08-30 21:56:03 -0400

    estructura y eventos

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$("#agregar_tv").click(function (){    
+    $('#tv').append("tv ");
+});   
+$("#agregar_notebook").click(function(){
+    $('#notebook').append("notebook ");
+    
+});
+$("#agregar_refri").click(function(){
+    $('#refrigerador').append("refrigerador ");
+    
+});
+$("#agregar_lavadora").click(function(){
+    $('#lavadora').append("Lavadora ");
+    
+});
+$("#agregar_cama").click(function(){
+    $('#cama').append("Cama");
+});
+$("#agregar_comoda").click(function(){
+    $('#comoda').append("comoda");
+});
+$("#agregar_smartphone").click(function(){
+    $('#smartphone').append("smartphone");
+});
+$("#agregar_cable").click(function(){
+    $('#cable').append("cable ");
+});
+$("#agregar_poleron").click(function(){
+    $('#poleron').append("poleron ");
+});
+$("#agregar_bici").click(function(){
+    $('#bici').append("bici ");
+});
